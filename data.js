c19d8392f7cb78f866de512b1c570d2e682f495b
Abdirizak14 <abdirizak52327@gmail.com>
2021-08-05 13:59:26 -0700

    commit
    commit

--- data.js
+++ data.js
@@ -1,16 +1,30 @@
 'use strict'
 
 let movies = [
-    {title: "sergio", director:"Greg Moura", type:"action", year:2019},
-    {title: "catfish", director:"Honry Joost", type:"adventure",year:2021},
-    {title: "the Queen", director:"Frank Simon",type:"romantic",year:2018},
-    {title: "Erth and blood", director:"julien", type:"action", year:2019},
-    {title: "The occupant", director:"Alex pastor", type:"scientific", year:2021},
+    {id:1, title: "sergio", director:"Greg Moura", type:"action", year:2019},
+    {id:2, title: "catfish", director:"Honry Joost", type:"adventure",year:2021},
+    {id:3, title: "the Queen", director:"Frank Simon",type:"romantic",year:2018},
+    {id:4, title: "Erth and blood", director:"julien", type:"action", year:2019},
+    {id:5, title: "The occupant", director:"Alex pastor", type:"scientific", year:2021},
 ];
 
-exports.getAll = function getAll() {
-    return movies;
-}
+// exports.getAll = function getAll() {
+//     return movies;
+// }
 //exports.getItem =title getItem() {
    // const movie =movies.find(movies => movies.director)  
 //}
+
+const getAll = () => {
+    console.log("Movies data: ", movies);
+    return {movies: true};
+}
+
+const getMovie = (id) => {
+    return movies.find(movie => movie.id == id);
+}
+
+module.exports = {
+    getAll,
+    getMovie
+}
--- index.js
+++ index.js
@@ -1,23 +1,56 @@
-var http = require("http");
-const movie = require('./data');
-var allMovies = movie.getAll();
+//var http = require("http");
 
-http.createServer(function(req,res){
-  var path = req.url.toLowerCase();
-  switch(path) {
-    case '/': 
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Home Page.' + 'these are my movies: ' + allMovies + ' movies');
-    break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About Me: My name is abdirizak. I study web development and I like play video games.');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('404:Page not found.');
-  }
+// var allMovies = movie.getAll();
+
+// http.createServer((req,res) =>{
+//   var path = req.url.toLowerCase();
+//   switch(path) {
+//     case '/': 
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.end('Home Page.' + 'these are my movies: ' + allMovies + ' movies');
+//     break;
+//     case '/about':
+//       res.writeHead(200, {'Content-Type': 'text/plain'});
+//       res.end('About Me: My name is abdirizak. I study web development and I like play video games.');
+//       break;
+//     default:
+//       res.writeHead(404, {'Content-Type': 'text/plain'});
+//       res.end('404:Page not found.');
+//   }
   
-}).listen(process.env.PORT || 3000);
+// }).listen(process.env.PORT || 3000);
 
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const movies = require('./data');
+const app = express();
+
+
+//Parsing the data
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}));
+
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs());
+//app.set('view engine', 'handlebars');⸀
+
+app.use(express.json());
+// set Access-Control-Allow-Origin header for api route
+var cors = require('cors')
+app.use(cors());
+
+//Send a GET request to READ(view) a list of quotes
+//http://localhost:3000/quotes/
+app.get('/', (req, res, next) => {
+    return movies.getAll()
+});
+
+//Send a GET request to READ(view) a single quote
+//http://localhost:3000/detail?id=
+app.get('/detail', (req, res) => {
+  const quote = movies.getMovie(req.params.id);
+});
+
+app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
